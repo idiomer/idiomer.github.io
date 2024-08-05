405bf4325e1b80f0ba9b4fc801e7b24dfb0ae11b
idiomer <idiomer>
2024-08-05 20:24:36 +0800

    Site updated: 2024-08-05 20:24:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/05/hello-world/","2024/08/05/Hexo博客搭建01-github-pages托管和自定义域名/"];function toRandomPost(){
+var posts=["2024/08/05/Hexo博客搭建01-github-pages托管和自定义域名/","2024/08/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
