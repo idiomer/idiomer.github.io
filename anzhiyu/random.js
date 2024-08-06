f98d0f7a39cc2b9712b41bbebd636bd8ab4a98dc
idiomer <idiomer>
2024-08-06 09:54:34 +0800

    Site updated: 2024-08-06 09:54:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/HelloWorld/","p/HexoBlog01/"];function toRandomPost(){
+var posts=["p/HexoBlog02/","p/HexoBlog01/","p/HelloWorld/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
