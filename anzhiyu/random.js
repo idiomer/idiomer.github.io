c9b99458932225653199001dfe884e55e4f70d70
ttxy <ttxy>
2025-01-06 13:39:53 +0800

    Site updated: 2025-01-06 13:39:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/HexoBlog02/","p/Hive和Spark常用代码片段/","p/Superset数据看板平台搭建/","p/HexoBlog01/","p/VSCode常用插件/"];function toRandomPost(){
+var posts=["p/HexoBlog01/","p/HexoBlog02/","p/Hive和Spark常用代码片段/","p/Superset数据看板平台搭建/","p/VSCode常用插件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
