34cd687859a344f1b2d9dc7fa64c856940353147
ttxy <ttxy>
2025-01-20 13:49:31 +0800

    Site updated: 2025-01-20 13:49:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/HexoBlog02/","p/HexoBlog01/","p/Hive和Spark常用代码片段/","p/Python与Jupyter的常用代码片段/","p/跨平台框架React Native(Expo)学习笔记/","p/Superset数据看板平台搭建/","p/VSCode常用插件/","p/docker教程/","p/免费翻译API/","p/用CNCLIP模型做内容理解/"];function toRandomPost(){
+var posts=["p/HexoBlog01/","p/HexoBlog02/","p/Hive和Spark常用代码片段/","p/Python与Jupyter的常用代码片段/","p/跨平台框架React Native(Expo)学习笔记/","p/VSCode常用插件/","p/Superset数据看板平台搭建/","p/免费翻译API/","p/docker教程/","p/用CNCLIP模型做内容理解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
