c90e8b44d14f720229452aab0a2f0ef1fa2f85e7
ttxy <ttxy>
2025-02-13 19:13:01 +0800

    Site updated: 2025-02-13 19:13:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["p/Bash常用配置/","p/HexoBlog01/","p/HexoBlog02/","p/Hive和Spark常用代码片段/","p/Python与Jupyter的常用代码片段/","p/跨平台框架React Native(Expo)学习笔记/","p/Superset数据看板平台搭建/","p/VSCode常用插件/","p/docker教程/","p/ollama使用教程/","p/免费翻译API/","p/用CNCLIP模型做内容理解/"];function toRandomPost(){
+var posts=["p/Bash常用配置/","p/HexoBlog02/","p/Hive和Spark常用代码片段/","p/Python与Jupyter的常用代码片段/","p/跨平台框架React Native(Expo)学习笔记/","p/HexoBlog01/","p/VSCode常用插件/","p/Superset数据看板平台搭建/","p/ollama使用教程/","p/docker教程/","p/免费翻译API/","p/用CNCLIP模型做内容理解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
